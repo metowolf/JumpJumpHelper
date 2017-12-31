fea09cdde589e304161a14656d2a559ac029da53
meto <i@i-meto.com>
2017-12-31 15:22:40 +0800

    :christmas_tree: 最好的语言

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # JumpJumpHelper
-用 PHP 玩微信跳一跳
+用~~最好的语言~~ PHP 玩微信跳一跳
+
+## 依赖
+
+ - php-cli (>=5.6)
+ - php-gd
+ - adb 调试工具
+ - android 手机
+
+## 食用方式
+
+ 1. 手机进入设置 > 开发者选项，打开 USB 调试、模拟触控
+ 2. 手机连接电脑，安装 adb 相关驱动，检查 `adb devices` 命令是否能显示设备 ID
+ 3. 微信进入跳一跳游戏，点击开始
+ 4. 电脑运行 `php run.php`
+
+如果发现跳跃过远/近，可以适当调节 `config.php` 中的参数
+
+## 效果图
+
+![效果图](https://i.loli.net/2017/12/31/5a488c9429845.png)
+
+
+## 脚本原理
+
+ 1. 截取手机屏幕，并传到电脑上。
+ 2. 通过分析图片，取得当前任务及其目标位置
+ 3. 计算按压时间，通过 adb 命令模拟点按操作
+
+## 识别原理
+
+ - 人物位置：通过训练数据匹配人物底座最宽的部分
+ - 目标位置：排除背景色后，从上至下找到第一个单峰位置，但遇到一些特殊情况会失败，待完善（所有识别数据储存在 screen 文件夹中）
+
+
+## 参考资料
+
+|项目|作者|
+|---|---|
+|[教你用Python来玩微信跳一跳](https://github.com/wangshub/wechat_jump_game)|[@wangshub](https://github.com/wangshub)|
+|[微信跳一跳自动玩耍工具](https://github.com/aOrz/wx_jump_game)|[@aOrz](https://github.com/aOrz)|
---
+++ run.php
@@ -0,0 +1,115 @@
+<?php
+
+require 'config.php';
+
+function similar($rgb1, $rgb2) {
+	$r1 = ($rgb1 >> 16) & 0xFF;
+	$g1 = ($rgb1 >> 8) & 0xFF;
+	$b1 = $rgb1 & 0xFF;
+	$r2 = ($rgb2 >> 16) & 0xFF;
+	$g2 = ($rgb2 >> 8) & 0xFF;
+	$b2 = $rgb2 & 0xFF;
+	return abs($r1 - $r2) < 10 && abs($b1 - $b2) < 10 && abs($g1 - $g2) < 10;
+}
+
+function getStart() {
+	global $image;
+	$width  = imagesx($image);
+	$height = imagesy($image);
+	for ($i = $height / 3; $i < $height / 3 * 2; $i++) {
+		for ($j = 0; $j < $width - 75; $j++) {
+			if (checkStart($i, $j)) {
+				$x = $i;
+				$y = $j + 37;
+			}
+		}
+	}
+	return array($x, $y);
+}
+
+function getEnd() {
+	global $image;
+	global $sx, $sy;
+	$width  = imagesx($image);
+	$height = imagesy($image);
+	for ($i = $height / 3; $i < $sx; $i++) {
+		$demo  = imagecolorat($image, 0, $i);
+		$count = 0;
+		for ($j = 0; $j < $width; $j++) {
+			$c = imagecolorat($image, $j, $i);
+			if (!similar($c, $demo)) {
+				$count++;
+			} else {
+				if ($count > BODY_WIDTH * 1.1) {
+					$x = $i;
+					$y = $j - $count / 2;
+					if (abs($y - $sy) > 20) {
+						return [$x, $y];
+					}
+				}
+				$count = 0;
+			}
+		}
+	}
+	return [0, 0];
+}
+
+$cheet = [
+    2829129, 2829129, 2829129, 2829129, 2829387, 2960716, 2960461,
+    2960463, 3026256, 3026256, 3092563, 3092563, 3158614, 3158615,
+    3224408, 3290202, 3356252, 3356252, 3356252, 3356767, 3357024,
+    3553376, 3618913, 3618913, 3553891, 3684707, 3750243, 3684706,
+    3684707, 3684707, 3684707, 3750243, 3618914, 3618913, 3684450,
+    3750243, 3750243, 3750243, 3750243, 3750243, 3684706, 3618913,
+    3618913, 3618913, 3684450, 3684706, 3684706, 3684450, 3684450,
+    3750243, 3750243, 3684450, 3684706, 3750243, 3618913, 3618913,
+    3618913, 3684448, 3749727, 3749727, 3749727, 3749727, 3749727,
+    3749470, 3749469, 3749469, 3749721, 3749720, 3749720, 3814998,
+    3814997, 3880534, 3880533, 3946835, 3881042
+];
+
+function checkStart($sx, $sy) {
+	global $cheet;
+	global $image;
+	for ($i = 0; $i < 75; $i++) {
+		$rgb = imagecolorat($image, $sy + $i, $sx);
+		if (!similar($rgb, $cheet[$i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function screencap() {
+    ob_start();
+	system('adb shell screencap -p /sdcard/screen.png');
+	system('adb pull /sdcard/screen.png .');
+    ob_end_clean();
+}
+
+function press($time) {
+    system('adb shell input swipe 500 1600 500 1601 ' . $time);
+}
+
+for ($id = 0; ; $id++) {
+    echo sprintf("#%05d: ", $id);
+    // 截图
+	screencap();
+    // 获取坐标
+	$image = imagecreatefrompng('screen.png');
+	list($sx, $sy) = getStart();
+	list($tx, $ty) = getEnd();
+    if ($sx == 0) break;
+	echo sprintf("(%d, %d) -> (%d, %d) ", $sx, $sy, $tx, $ty);
+    // 图像描点
+	imagefilledellipse($image, $sy, $sx, 10, 10, 0xFF0000);
+	imagefilledellipse($image, $ty, $tx, 10, 10, 0xFF0000);
+	imagepng($image, sprintf("screen/%05d.png", $id));
+    // 计算按压时间
+	$time = sqrt(pow($tx - $sx, 2) + pow($ty - $sy, 2)) * PRESS_TIME;
+	$time = round(max(200, $time));
+    echo sprintf("time: %f\n", $time);
+	press($time);
+    // 等待下一次截图
+	sleep(SLEEP_TIME);
+}
