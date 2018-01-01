31dd234f51e05bdadaba62812926a8917c65b710
meto <i@i-meto.com>
2018-01-01 21:50:04 +0800

    :shaved_ice: Mathematica 拟合，屏幕分辨率修复

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
  * 按压力度参数，根据实际表现进行调节
  * 如果跳远了就调低点
  */
-define('PRESS_TIME', 3.752);
+define('PRESS_TIME', 4.210926);
 /**
  * 睡眠时间
  */
-define('SLEEP_TIME', 2);
+define('SLEEP_TIME', 1.5);
 /**
- * 角色宽度，不需要调节
+ * 角色宽度，根据需要调节
  */
 define('BODY_WIDTH', 75);
--- run.php
+++ run.php
@@ -2,25 +2,40 @@
 
 require 'config.php';
 
-function similar($rgb1, $rgb2) {
+function similar($rgb1, $rgb2, $value = 10) {
 	$r1 = ($rgb1 >> 16) & 0xFF;
 	$g1 = ($rgb1 >> 8) & 0xFF;
 	$b1 = $rgb1 & 0xFF;
 	$r2 = ($rgb2 >> 16) & 0xFF;
 	$g2 = ($rgb2 >> 8) & 0xFF;
 	$b2 = $rgb2 & 0xFF;
-	return abs($r1 - $r2) < 10 && abs($b1 - $b2) < 10 && abs($g1 - $g2) < 10;
+	return abs($r1 - $r2) < $value && abs($b1 - $b2) < $value && abs($g1 - $g2) < $value;
 }
 
 function getStart() {
 	global $image;
+	$l_r    = 0;
+	$cnt    = 0;
 	$width  = imagesx($image);
 	$height = imagesy($image);
-	for ($i = $height / 3; $i < $height / 3 * 2; $i++) {
-		for ($j = 0; $j < $width - 75; $j++) {
-			if (checkStart($i, $j)) {
-				$x = $i;
-				$y = $j + 37;
+	for ($i = $height / 3 * 2; $i > $height / 3; $i--) {
+		for ($l = 0; $l < $width; $l++) {
+			$c = imagecolorat($image, $l, $i);
+			if (similar($c, 3750243, 20)) {
+				$r = $l;
+				while($r+1 < $width && similar(imagecolorat($image, $r+1, $i), 3750243, 20)){
+					$r++;
+				}
+				if ($r - $l > BODY_WIDTH * 0.5){
+					if ($r <= $l_r) {
+						return [$i, round(($l + $r) / 2)];
+					}
+					else {
+						$cnt = 0;
+					}
+					$l_r = $r;
+				}
+				$l = $r;
 			}
 		}
 	}
@@ -66,32 +81,6 @@ function getEnd() {
 	return [$sx - round(abs($mid-$sy)/sqrt(3)), round($mid)];;
 }
 
-$cheet = [
-    2829129, 2829129, 2829129, 2829129, 2829387, 2960716, 2960461,
-    2960463, 3026256, 3026256, 3092563, 3092563, 3158614, 3158615,
-    3224408, 3290202, 3356252, 3356252, 3356252, 3356767, 3357024,
-    3553376, 3618913, 3618913, 3553891, 3684707, 3750243, 3684706,
-    3684707, 3684707, 3684707, 3750243, 3618914, 3618913, 3684450,
-    3750243, 3750243, 3750243, 3750243, 3750243, 3684706, 3618913,
-    3618913, 3618913, 3684450, 3684706, 3684706, 3684450, 3684450,
-    3750243, 3750243, 3684450, 3684706, 3750243, 3618913, 3618913,
-    3618913, 3684448, 3749727, 3749727, 3749727, 3749727, 3749727,
-    3749470, 3749469, 3749469, 3749721, 3749720, 3749720, 3814998,
-    3814997, 3880534, 3880533, 3946835, 3881042
-];
-
-function checkStart($sx, $sy) {
-	global $cheet;
-	global $image;
-	for ($i = 0; $i < 75; $i++) {
-		$rgb = imagecolorat($image, $sy + $i, $sx);
-		if (!similar($rgb, $cheet[$i])) {
-			return false;
-		}
-	}
-	return true;
-}
-
 function screencap() {
     ob_start();
 	system('adb shell screencap -p /sdcard/screen.png');
@@ -116,11 +105,13 @@ for ($id = 0; ; $id++) {
     // 图像描点
 	imagefilledellipse($image, $sy, $sx, 10, 10, 0xFF0000);
 	imagefilledellipse($image, $ty, $tx, 10, 10, 0xFF0000);
+	imagepng($image, 'screen.scan.png');
 	imagepng($image, sprintf("screen/%05d.png", $id));
     // 计算按压时间
-	$time = pow(pow($tx - $sx, 2) + pow($ty - $sy, 2), 0.425) * PRESS_TIME;
+	$dist = sqrt(pow($tx - $sx, 2) + pow($ty - $sy, 2));
+	$time = pow($dist, 0.83676) * PRESS_TIME;
 	$time = round($time);
-    echo sprintf("time: %f\n", $time);
+    echo sprintf("dist: %f, time: %f\n", $dist, $time);
 	press($time);
     // 等待下一次截图
 	sleep(SLEEP_TIME);
